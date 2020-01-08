0df96ba797030a26db3e2a81d0013093a796e922
Leo <losoviz@gmail.com>
2020-01-08 18:04:58 +0800

    Fixed issues

--- src/Hooks/VarsHooks.php
+++ src/Hooks/VarsHooks.php
@@ -1,5 +1,5 @@
 <?php
-namespace PoP\APIGraphQLRequest\Hooks;
+namespace PoP\GraphQLAPIRequest\Hooks;
 
 use PoP\API\Schema\QueryInputs;
 use PoP\Engine\Hooks\AbstractHookSet;
@@ -30,8 +30,15 @@ class VarsHooks extends AbstractHookSet
         // If the "query" param is set, this case is already handled in API package
         if (!isset($_REQUEST[QueryInputs::QUERY])) {
             // Attempt to get the query from the body, following the GraphQL syntax
-            // TODO: Get $payload
             $payload = false;
+            // if (isset($_SERVER['CONTENT_TYPE']) && $_SERVER['CONTENT_TYPE'] === 'application/json') {
+            //     $rawBody     = file_get_contents('php://input');
+            //     $requestData = json_decode($rawBody ?: '', true);
+            // } else {
+            //     $requestData = $_POST;
+            // }
+            // $payload = isset($requestData['query']) ? $requestData['query'] : null;
+            // $variables = isset($requestData['variables']) ? $requestData['variables'] : null;
             if ($payload) {
                 // The fields param can either be an array or a string. Convert them to array
                 $vars['query'] = FieldQueryConvertorUtils::getQueryAsArray($payload);
