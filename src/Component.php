1ea97ed1377050a623029f7b06d772c2c7cec7bb
Leo <losoviz@gmail.com>
2020-04-13 19:07:32 +0800

    Added declare(strict_types=1);

--- src/Component.php
+++ src/Component.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\GraphQLAPIRequest;
 
 use PoP\Root\Component\AbstractComponent;
--- src/Hooks/VarsHooks.php
+++ src/Hooks/VarsHooks.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\GraphQLAPIRequest\Hooks;
 
 use PoP\API\Schema\QueryInputs;
